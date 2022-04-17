034d02add0c988fd1092898b42f08436114531ce
Scott <hudsonsc@oregonstate.edu>
2022-04-17 11:33:48 -0600

    Set up base route, got uikit working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const ROUTES: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    }
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(ROUTES)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
